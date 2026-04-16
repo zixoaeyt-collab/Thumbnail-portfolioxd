cb100a1f44e86b0618ff09412c4bf4602af51286
zixoaeyt-collab <zixoaeyt@gmail.com>
2026-04-16 17:43:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Thumbnail-portfolioxd/',
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
