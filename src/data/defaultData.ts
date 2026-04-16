b4d074ca2fb7422ed0c44f84d59ae773eb055413
zixoaeyt-collab <zixoaeyt@gmail.com>
2026-04-16 17:48:11 +0530

    Update defaultData.ts

--- src/data/defaultData.ts
+++ src/data/defaultData.ts
@@ -1,30 +1,93 @@
 import { PortfolioData } from '../types';
 
 export const defaultPortfolioData: PortfolioData = {
-  name: 'UNZAIXO',
-  tagline: 'DIGITAL THUMBNAILS/GRAPHICS DESIGNER',
-  logoUrl: '',
-  thumbnails: [],
-  skills: [
-    { id: '1', name: 'UI/UX Design', percentage: 30 },
-    { id: '2', name: 'Graphic Design', percentage: 70 },
-    { id: '3', name: 'Motion Graphics', percentage: 25 },
-    { id: '4', name: '3D Modeling', percentage: 15 },
-    { id: '5', name: 'Illustration', percentage: 85 },
-    { id: '6', name: 'Brand Identity', percentage: 85 },
-    { id: '7', name: 'Web Development', percentage: 40 },
-    { id: '8', name: 'Typography', percentage: 65 },
+  {
+  "name": "UNZAIXO",
+  "tagline": "DIGITAL THUMBNAILS/GRAPHICS DESIGNER",
+  "logoUrl": "",
+  "thumbnails": [],
+  "skills": [
+    {
+      "id": "1",
+      "name": "UI/UX Design",
+      "percentage": 30
+    },
+    {
+      "id": "2",
+      "name": "Graphic Design",
+      "percentage": 70
+    },
+    {
+      "id": "3",
+      "name": "Motion Graphics",
+      "percentage": 25
+    },
+    {
+      "id": "4",
+      "name": "3D Modeling",
+      "percentage": 15
+    },
+    {
+      "id": "5",
+      "name": "Illustration",
+      "percentage": 85
+    },
+    {
+      "id": "6",
+      "name": "Brand Identity",
+      "percentage": 85
+    },
+    {
+      "id": "7",
+      "name": "Web Development",
+      "percentage": 40
+    },
+    {
+      "id": "8",
+      "name": "Typography",
+      "percentage": 65
+    }
   ],
-  contact: {
-    email: 'Z',
-    phone: '',
-    socials: [
-      { id: '1', platform: 'Instagram', url: 'https://instagram.com', icon: 'instagram' },
-      { id: '2', platform: 'Twitter/X', url: 'https://x.com', icon: 'twitter' },
-      { id: '3', platform: 'YouTube', url: 'https://youtube.com', icon: 'youtube' },
-      { id: '4', platform: 'GitHub', url: 'https://github.com', icon: 'github' },
-      { id: '5', platform: 'Discord', url: 'https://discord.com', icon: 'discord' },
-      { id: '6', platform: 'Behance', url: 'https://behance.net', icon: 'behance' },
-    ],
-  },
-};
+  "contact": {
+    "email": "Z",
+    "phone": "",
+    "socials": [
+      {
+        "id": "1",
+        "platform": "Instagram",
+        "url": "https://instagram.com",
+        "icon": "instagram"
+      },
+      {
+        "id": "2",
+        "platform": "Twitter/X",
+        "url": "https://x.com",
+        "icon": "twitter"
+      },
+      {
+        "id": "3",
+        "platform": "YouTube",
+        "url": "https://youtube.com",
+        "icon": "youtube"
+      },
+      {
+        "id": "4",
+        "platform": "GitHub",
+        "url": "https://github.com",
+        "icon": "github"
+      },
+      {
+        "id": "5",
+        "platform": "Discord",
+        "url": "https://discord.com",
+        "icon": "discord"
+      },
+      {
+        "id": "6",
+        "platform": "Behance",
+        "url": "https://behance.net",
+        "icon": "behance"
+      }
+    ]
+  }
+}
